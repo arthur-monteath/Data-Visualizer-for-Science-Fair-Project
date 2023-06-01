2394e7bc5facfbc68df6d5444e5f2235eeb0a3a1
Arthur Trindade <thucatrindade1@gmail.com>
2023-05-31 23:05:07 -0300

    Transfer to Laptop
    No notable changes.

--- Assets/Scripts/DataInterpolator.cs
+++ Assets/Scripts/DataInterpolator.cs
@@ -10,14 +10,31 @@ public class DataInterpolator : MonoBehaviour
     {
         for(int i = 0; i < data.Length; i++)
         {
-            if (data[i] != null)
+            if (data[i] == null)
             {
-                Interpolate(i);
+                InterpolateMissingData(i);
             }
         }
     }
 
-    private void Interpolate(int i)
+    private void InterpolateMissingData(int i)
+    {
+        DataSO minData = null, maxData = null;
+
+        for(int q = i-1; q > 0; q--)
+        {
+            if (data[q] != null) minData = data[q];
+        }
+
+        for(int p = i+1; p < data.Length; p++)
+        {
+            if (data[p] != null) maxData = data[p];
+        }
+        
+        InterpolateValues(minData, maxData, i);
+    }
+
+    private void InterpolateValues(DataSO minData, DataSO maxData, int index)
     {
 
     }
